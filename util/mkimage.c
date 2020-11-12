186e7da4f79d79a725068ed803c93b64ca4bae0a
Peter Jones <pjones@redhat.com>
2020-11-12 10:41:33 -0500

    mkimage: --sbat to import SBAT metadata into a .sbat section
    Add a --sbat option to the grub-mkimage tool that allows to import a
    SBAT metadata as a CSV file into a .sbat section of the PE32+ binary.
    
    Signed-off-by: Peter Jones <pjones@redhat.com>
    Signed-off-by: Javier Martinez Canillas <javierm@redhat.com>

--- util/mkimage.c
+++ util/mkimage.c
@@ -860,12 +860,13 @@ grub_install_generate_image (const char *dir, const char *prefix,
 			     char *memdisk_path, char **pubkey_paths,
 			     size_t npubkeys, char *config_path,
 			     const struct grub_install_image_target_desc *image_target,
-			     int note, grub_compression_t comp, const char *dtb_path)
+			     int note, grub_compression_t comp, const char *dtb_path,
+			     const char *sbat_path)
 {
   char *kernel_img, *core_img;
   size_t total_module_size, core_size;
   size_t memdisk_size = 0, config_size = 0;
-  size_t prefix_size = 0, dtb_size = 0;
+  size_t prefix_size = 0, dtb_size = 0, sbat_size = 0;
   char *kernel_path;
   size_t offset;
   struct grub_util_path_list *path_list, *p;
@@ -1281,6 +1282,7 @@ grub_install_generate_image (const char *dir, const char *prefix,
     case IMAGE_EFI:
       {
 	char *pe_img;
+	char *pe_sbat;
 	char *header;
 	struct grub_pe32_section_table *section;
 	size_t n_sections = 4;
@@ -1299,8 +1301,19 @@ grub_install_generate_image (const char *dir, const char *prefix,
 	else
 	  header_size = EFI64_HEADER_SIZE;
 
+	if (sbat_path != NULL)
+	  {
+	    if (image_target->id != IMAGE_EFI)
+	      grub_util_error (_("sbat section can only be embedded in EFI images."));
+
+	    sbat_size = ALIGN_ADDR(grub_util_get_image_size (sbat_path));
+	  }
+
 	pe_size = ALIGN_UP (header_size + core_size, GRUB_PE32_FILE_ALIGNMENT)
 		  + ALIGN_UP (layout.reloc_size, GRUB_PE32_FILE_ALIGNMENT);
+	if (sbat_size)
+	  pe_size += ALIGN_UP(sbat_size, GRUB_PE32_FILE_ALIGNMENT);
+
 	header = pe_img = xcalloc (1, pe_size);
 	vma = raw_data = header_size;
 
@@ -1314,6 +1327,9 @@ grub_install_generate_image (const char *dir, const char *prefix,
 	c = (struct grub_pe32_coff_header *)
 	    (header + GRUB_PE32_MSDOS_STUB_SIZE + GRUB_PE32_SIGNATURE_SIZE);
 	c->machine = grub_host_to_target16 (image_target->pe_target);
+
+	if (sbat_path)
+	  n_sections += 1;
 
 	c->num_sections = grub_host_to_target16 (n_sections);
 	c->time = grub_host_to_target32 (STABLE_EMBEDDING_TIMESTAMP);
@@ -1415,6 +1431,26 @@ grub_install_generate_image (const char *dir, const char *prefix,
 				   GRUB_PE32_SCN_CNT_INITIALIZED_DATA
 				   | GRUB_PE32_SCN_MEM_READ
 				   | GRUB_PE32_SCN_MEM_WRITE);
+
+	if (sbat_path)
+	  {
+	    grub_uint32_t hdr_size = 0;
+	    scn_size = sbat_size;
+
+	    pe_sbat = pe_img + raw_data + hdr_size;
+	    grub_util_load_image (sbat_path, pe_sbat);
+
+	    PE_OHDR(o32, o64, resource_table.rva) =
+					    grub_host_to_target32 (raw_data);
+	    PE_OHDR(o32, o64, resource_table.size) =
+					    grub_host_to_target32 (scn_size);
+	    section = init_pe_section (image_target, section, ".sbat",
+				    &vma, scn_size,
+				    image_target->section_align,
+				    &raw_data, scn_size,
+				    GRUB_PE32_SCN_CNT_INITIALIZED_DATA
+				    | GRUB_PE32_SCN_MEM_READ);
+	  }
 
 	scn_size = layout.reloc_size;
 	PE_OHDR(o32, o64, base_relocation_table.rva) = grub_host_to_target32 (vma);
